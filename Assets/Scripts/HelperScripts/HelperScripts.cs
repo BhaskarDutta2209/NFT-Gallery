6f18faecc10a182df47a9e256776d9c9be5a0da9
Bhaskar Dutta <bhaskardutta2209@gmail.com>
2021-08-23 00:19:51 +0530

    making API calls from project

---
+++ Assets/Scripts/HelperScripts/HelperScripts.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse); 
+public class Attribute
+{
+    public string trait_type { get; set; }
+    public object value { get; set; }
+    public string display_type { get; set; }
+}
+
+public class OpenSeaMetadata
+{
+    public List<Attribute> attributes { get; set; }
+    public string description { get; set; }
+    public string external_url { get; set; }
+    public string image { get; set; }
+    public string name { get; set; }
+}
+
---
+++ Assets/Scripts/WebRequests/TestGetRequest.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using Newtonsoft.Json;
+
+public class TestGetRequest : MonoBehaviour
+{
+
+    public Plane imagePlane;
+    private float progress;
+
+    public string url;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(GetRequestCoroutine());
+    }
+
+    private IEnumerator GetRequestCoroutine()
+    {
+        UnityWebRequest www = UnityWebRequest.Get(url);
+        var asyncOperation = www.SendWebRequest();
+
+        while(!www.isDone)
+        {
+            progress = asyncOperation.progress;
+            yield return null;
+        }
+
+        progress = 1f;
+
+        if(!string.IsNullOrEmpty(www.error))
+        {
+            Debug.Log("Some Error Occured");
+        }
+
+        var result = www.downloadHandler.text;
+
+        //var metadata = JsonUtility.FromJson<OpenSeaMetadata>(result);
+        var metadata = JsonConvert.DeserializeObject<OpenSeaMetadata>(result);
+
+        //Debug.Log(metadata.attributes[0].trait_type);
+
+        //Debug.Log(result);
+        //Debug.Log(result.GetType());
+        //Debug.Log(openSeaMetadata.name);
+        Debug.Log(metadata.attributes[0].trait_type);
+        //Debug.Log(openSeaMetadata.image);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
