f327669fe2fe72726db03b824f93e6fcedd9dc0a
Bhaskar Dutta <bhaskardutta2209@gmail.com>
2021-08-27 18:07:54 +0530

    interacting with the smart contracts

---
+++ Assets/Scripts/Test/Test.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    async void Start()
+    {
+        QueryArtMarket queryArtMarket = new QueryArtMarket();
+        var res = await queryArtMarket.GetItemCounter();
+        Debug.Log(res);
+        var res2 = await queryArtMarket.getAllMarketItems();
+        Debug.Log(res2[0].nftContractAddress);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
